a18807ff13a9fef66ba457c8e5e8acd15fb27dc4
Drew Westrick <drewwestrick@glassboard.com>
2017-06-22 22:38:38 -0400

    Added vendor assets to allow js to run properly.

--- app.js
+++ app.js
@@ -16,5 +16,6 @@ module.exports = {
     minify: env === 'production',
     warnForDuplicates: env !== 'production'
   }),
-  babel: jsStandards()
+  babel: jsStandards(),
+  vendor: 'assets/js/**'
 }
